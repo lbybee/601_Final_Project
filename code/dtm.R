6ceff25df6b32dfb1f5001a04822853eaeea64e8
Roger Fan <rogerfan@umich.edu>
2016-04-08 18:18:03 -0400

    Processing senders data as well.

--- code/dtm.R
+++ code/dtm.R
@@ -2,26 +2,30 @@ library(tm)
 library(SnowballC)
 
 
-tab <- read.csv("../data/emails.csv")
+tab = read.csv("../data/emails.csv")
+date = as.Date(tab$DateSent)
 
-date <- as.Date(tab$DateSent)
-tab <- tab[!is.na(date),]
-date <- date[!is.na(date)]
-vect <- VectorSource(tab$AllText)
-corpus <- Corpus(vect)
-corpus <- tm_map(corpus, content_transformer(tolower))
-corpus <- tm_map(corpus, removePunctuation)
-corpus <- tm_map(corpus, removeNumbers)
-corpus <- tm_map(corpus, removeWords, stopwords("english"))
-corpus <- tm_map(corpus, stemDocument)
-corpus <- tm_map(corpus, stripWhitespace)
-meta(corpus, tag="date") <- date
-dtm <- DocumentTermMatrix(corpus)
-dtm <- removeSparseTerms(dtm, 0.999)
+tab = tab[!is.na(date),]
+date = date[!is.na(date)]
+senders = tab[c('SenderPerson', 'SenderPersonId')]
 
-totals <- apply(dtm, 1, sum)
-dtm <- dtm[totals > 0,]
-dates <- date[totals > 0]
+vect = VectorSource(tab$AllText)
+corpus = Corpus(vect)
+corpus = tm_map(corpus, content_transformer(tolower))
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeNumbers)
+corpus = tm_map(corpus, removeWords, stopwords("english"))
+corpus = tm_map(corpus, stemDocument)
+corpus = tm_map(corpus, stripWhitespace)
+meta(corpus, tag="date") = date
+dtm = DocumentTermMatrix(corpus)
+dtm = removeSparseTerms(dtm, 0.999)
 
+totals = apply(dtm, 1, sum)
+dtm = dtm[totals > 0,]
+dates = date[totals > 0]
+senders = senders[totals > 0,]
+
+save(senders, file="../data/senders.rda")
 save(dates, file="../data/dates.rda")
 save(dtm, file="../data/dtm.rda")
