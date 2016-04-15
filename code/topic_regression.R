6814db1ad8148eb330063c7254934389cc2603ff
lbybee <leland.bybee@gmail.com>
2016-04-14 21:53:47 -0400

    update coefficient plot

--- code/topic_regression.R
+++ code/topic_regression.R
@@ -3,6 +3,8 @@ library(glmnet)
 library(nnet)
 library(ggplot2)
 library(reshape2)
+library(RColorBrewer)
+require(extrafont)
 
 K <- 30
 
@@ -13,6 +15,8 @@ people <- table(senders$SenderPerson)
 people <- names(people[people > 100])
 people <- people[people != ""]
 
+#temp <- model@gamma
+#temp <- sweep(model@gamma, 2, apply(model@gamma, 2, mean))
 temp <- scale(model@gamma)
 data <- data.frame(temp)
 data$Person <- senders$SenderPerson
@@ -46,12 +50,23 @@ for(i in u_names){
 
 coefficients <- data.frame(coef(fit))
 coefficients <- coefficients[,2:(K+1)]
-coefficients[coefficients < 0] <- 0
+#coefficients[coefficients < -1] <- -1
+#coefficients[coefficients < 0] <- 0
 #coefficients <- scale(coefficients)
-for(i in 1:K){
-    coefficients[,i] <- (coefficients[,i] - min(coefficients[,i])) / (max(coefficients[,i]) - min(coefficients[,i]) + 0.0000001)
-}
+#for(i in 1:K){
+#    coefficients[,i] <- (coefficients[,i] - min(coefficients[,i])) / (max(coefficients[,i]) - min(coefficients[,i]) + 0.0000001) 
+#}
+test <- c("Meetings", "Middle East", "Staff", "Politics", "Terrorism", "Foreign Policy", "Press", "Hillary", "Communication", "Common")
+col <- c('#e31a1c','#1f78b4','#a6cee3','#33a02c','#b2df8a','#fb9a99','#ff7f00','#6a3d9a','#cab2d6','#fdbf6f')
+sortorder <- c(5, 22, 6, 18, 19, 27, 29, 2, 7, 13, 21, 23, 26, 11, 17, 25, 16, 20, 4, 12, 9, 10, 24, 28, 30, 1, 3, 8, 14, 15)
+colororder <- c(rep(col[5], 2), rep(col[2], 5), rep(col[6], 6), rep(col[4], 3), rep(col[3], 2), rep(col[7], 1), rep(col[8], 1), rep(col[1], 5), rep(col[9], 1), rep(col[10], 4))
+coefficients <- coefficients[,sortorder]
+coefficients[coefficients < -0.9] <- -0.9
+coefficients[coefficients > 0.9] <- 0.9 
 mlt <- melt(t(t(coefficients)))
-colnames(mlt) <- c("Sender", "Topic", "value")
-ggplot(data=mlt) + geom_tile(aes(x=Topic, y=Sender, fill=value)) + theme(legend.position = "none", axis.text.x = element_text(angle = 90, hjust = 0))
+colnames(mlt) <- c("Sender", "Topic", "Coefficient")
+cols <- rev(brewer.pal(11, 'PiYG'))
+#4D9221
+#C51B7D
+ggplot(data=mlt, aes(x=Topic, y=Sender)) + geom_tile(aes(fill=Coefficient), colour="white") + scale_fill_gradient2(midpoint=median(as.matrix(coefficients)), low="#00CDCD", high="#CC00CC") + theme(text=element_text(family="CM Roman"), axis.text.x=element_text(colour=colororder, angle = 90, hjust = 0))
 ggsave("coefficients.pdf")
